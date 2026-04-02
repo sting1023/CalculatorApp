69b6856cbb83fe50edfa48b7ca9dbfe26ffbcd28
sting1023 <128615011+sting1023@users.noreply.github.com>
2026-04-02 22:41:36 +0800

    Add app/src/main/java/com/sting/calculator/ui/theme/Color.kt

---
+++ app/src/main/java/com/sting/calculator/ui/theme/Color.kt
@@ -0,0 +1,33 @@
+package com.sting.calculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colors
+val LightBackground = Color(0xFFF5F5F7)
+val LightDisplayBackground = Color(0xFFFFFFFF)
+val LightNumberButtonBackground = Color(0xFFFFFFFF)
+val LightOperatorButtonBackground = Color(0xFFF0F0F0)
+val LightFunctionButtonBackground = Color(0xFFD4D4D2)
+val LightEqualsBackground = Color(0xFF007AFF)
+val LightResultText = Color(0xFF1C1C1E)
+val LightFormulaText = Color(0xFF636366)
+val LightButtonText = Color(0xFF1C1C1E)
+
+// Dark Theme Colors
+val DarkBackground = Color(0xFF1C1C1E)
+val DarkDisplayBackground = Color(0xFF2C2C2E)
+val DarkNumberButtonBackground = Color(0xFF3A3A3C)
+val DarkOperatorButtonBackground = Color(0xFF505050)
+val DarkFunctionButtonBackground = Color(0xFF636366)
+val DarkEqualsBackground = Color(0xFF0A84FF)
+val DarkResultText = Color(0xFFFFFFFF)
+val DarkFormulaText = Color(0xFFAEAEB2)
+val DarkButtonText = Color(0xFFFFFFFF)
+
+// Common Colors
+val Orange = Color(0xFFFF9500)
+val OrangeDark = Color(0xFFCC8000)
+val Blue = Color(0xFF007AFF)
+val BlueDark = Color(0xFF0A84FF)
+val Gray = Color(0xFFD4D4D2)
+val GrayDark = Color(0xFF636366)
